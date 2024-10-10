b9c299d8face3da9931a04ddb277ed9419210d3b
JonatanFD <acunacorahuajon@gmail.com>
2024-10-09 20:21:50 -0500

    getJourney added

--- src/services/services.ts
+++ src/services/services.ts
@@ -1,25 +1,42 @@
-import { CategoryStatus } from "../types";
+import { JourneyConstrains } from "../types";
 
 const API_URL = "http://127.0.0.1:5000/";
 
 export async function getCities() {
-    try {
-        const data = await fetch(API_URL + "cities");
-        const countries = await data.json();
+  try {
+    const data = await fetch(API_URL + "cities");
+    const countries = await data.json();
 
-        return countries;
-    } catch (error) {
-        console.log(error);
-    }
+    return countries;
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 export async function getRoutes() {
-    try {
-        const data = await fetch(API_URL + "routes");
-        const routes = await data.json();
+  try {
+    const data = await fetch(API_URL + "routes");
+    const routes = await data.json();
 
-        return routes;
-    } catch (error) {
-        console.log(error);
-    }
+    return routes;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function getJourney(journeyConstrains: JourneyConstrains) {
+  try {
+    const data = await fetch(API_URL + "journey", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(journeyConstrains),
+    });
+    
+    const journey = await data.json();
+    return journey;
+  } catch (error) {
+    console.log(error);
+  }
 }
